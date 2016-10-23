faaf92b537181bae7b0dce917e100539095c6164
Felipe Cardozo <pypelyncar@hotmail.com>
2016-10-23 20:23:03 +0200

    We can use other base package "com.gears", by default right now is com.security

--- backend/src/main/java/com/security/SpringSecurityDemoApplication.java
+++ backend/src/main/java/com/security/SpringSecurityDemoApplication.java
@@ -1,12 +1,10 @@
 package com.security;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+// @SpringBootApplication
+// @ComponentScan({"com.security"})
 public class SpringSecurityDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityDemoApplication.class, args);
-	}
+    // public static void main(String[] args) {
+    // // SpringApplication.run(SpringSecurityDemoApplication.class, args);
+    // }
 }
